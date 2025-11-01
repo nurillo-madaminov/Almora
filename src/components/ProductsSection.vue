515b5fa796848837625600dc8a3286a36dd7561c
Nurillo Madaminov <122042047+nurillo-madaminov@users.noreply.github.com>
2025-11-01 11:33:59 -0700

    bugs fixed

--- src/components/ProductsSection.vue
+++ src/components/ProductsSection.vue
@@ -6,7 +6,7 @@ const props = defineProps(['title', "products"])
 
 <template>
   <div>
-    <h1 class="ml-3 mb-1 font-semibold -shadow-lg">{{props.title}}</h1>
+    <h1 class="ml-3 mb-1 text-xl font-semibold">{{props.title}}</h1>
     <div class="grid grid-cols-2 gap-2 px-2 mb-5">
       <product-card v-for="product in products" :key="product"/>
     </div>
